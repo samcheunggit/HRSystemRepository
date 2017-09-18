206243972a2f5a4450144017b16c83319521549a
gogi.strat <gogi.strat@gmail.com>
2017-09-18 05:40:18 -0400

    new file

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 // server.js
 const express = require('express');
+const http = require('http');
+const path = require('path');
+
+const api = require('./server/routes/api');
+
 const app = express();
-// If an incoming request uses
-// a protocol other than HTTPS,
-// redirect that request to the
-// same url but with HTTPS
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
-// Instruct the app
-// to use the forceSSL
-// middleware
-app.use(forceSSL());
+
+app.use(express.static(path.join(_dirname, 'dist')));
+
+app.get('*', (req,res) => {
+  res.sendFile(path.join(_dirname, 'dist/index.html'));
+});
+
+const port = process.env.PORT || '3001';
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, ()=>console.log('Running'));
