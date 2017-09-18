3d61b346394f38d7dee9813bb77c462d341ab4a3
gogi.strat <gogi.strat@gmail.com>
2017-09-18 05:45:35 -0400

    new file

--- server.js
+++ server.js
@@ -1,20 +1,29 @@
-// server.js
+// // server.js
+// const express = require('express');
+// const http = require('http');
+// const path = require('path');
+
+// const api = require('./server/routes/api');
+
+// const app = express();
+
+// app.use(express.static(path.join(_dirname, 'dist')));
+
+// app.get('*', (req,res) => {
+//   res.sendFile(path.join(_dirname, 'dist/index.html'));
+// });
+
+// const port = process.env.PORT || '3001';
+// app.set('port', port);
+
+// const server = http.createServer(app);
+// server.listen(port, ()=>console.log('Running'));
+
 const express = require('express');
-const http = require('http');
-const path = require('path');
-
-const api = require('./server/routes/api');
-
 const app = express();
-
-app.use(express.static(path.join(_dirname, 'dist')));
-
-app.get('*', (req,res) => {
-  res.sendFile(path.join(_dirname, 'dist/index.html'));
-});
-
-const port = process.env.PORT || '3001';
-app.set('port', port);
-
-const server = http.createServer(app);
-server.listen(port, ()=>console.log('Running'));
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
