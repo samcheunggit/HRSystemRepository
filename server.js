bf910e862d6afb99d9757b9b9866289baaec4bbc
gogi.strat <gogi.strat@gmail.com>
2017-09-18 06:48:09 -0400

    new file

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 
-// const api = require('./server/routes/api');
+const api = require('./server/routes/api');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// app.get('*', (req,res) => {
-//   res.sendFile(path.join(_dirname, 'dist/index.html'));
-// });
+app.get('*', (req,res) => {
+  res.sendFile(path.join(_dirname, 'dist/index.html'));
+});
 
 const port = process.env.PORT || '8080';
 app.set('port', port);
